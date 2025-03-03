ce2ff3f229f58078293f4ae9bdabdd750d9a0cd9
Angerbode <jdr@frichs-pyrolysis.com>
2025-03-04 00:18:54 +0100

    added asteroidfields

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import time
 from constants import *
 from player import *
 from circleshape import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
@@ -12,7 +16,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
+
+    #bundeling stuff up
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+    asteroidField = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroid, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     #clock for determining the fps of the game
     dt = 0
     clock = pygame.time.Clock()
@@ -20,14 +33,21 @@ def main():
     #player
     objPlayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/2)
 
+    #asteroid field
+    objAsteroidfield = AsteroidField()
+
     #gameloop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        objPlayer.draw(screen)
 
+        pygame.Surface.fill(screen, (0, 0, 0))#clear screen
+
+        updatable.update(dt)
+
+        for unit in drawable:
+            unit.draw(screen) #draw onto screen
         pygame.display.flip()
         dt = clock.tick(60)/1000 #sets 60 fps and outputs it to dt converted to seconds
         print(f"{dt}", end="\r")
